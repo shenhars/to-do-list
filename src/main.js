1e943a1cc234b1573276d8152b75ccef7f51c5a4
shenhars <shenhars03@gmail.com>
2020-07-19 15:19:47 +0300

    add priority to list item and date and time fix

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 let addButton = document.querySelector("#addButton");
 let textInput = document.querySelector("#textInput");
 let toDoList = document.querySelector("#toDoList");
+let prioritySelector = document.querySelector("#prioritySelector");
 
 addButton.addEventListener("click", function() {
     let li = document.createElement("li");
 
     let container = document.createElement("div");
     container.setAttribute("class", "todoContainer");
+    
+    let todoPriority = document.createElement("span");
+    todoPriority.setAttribute('class', "todoPriority");
+    todoPriority.innerHTML = prioritySelector.value;
 
     let todoText = document.createElement("span");
     todoText.setAttribute("class", "todoText");
@@ -14,11 +19,12 @@ addButton.addEventListener("click", function() {
 
     let todoCreatedAt = document.createElement("span");
     let date = new Date();
-    todoText.setAttribute("class", "todoCreatedAt");
-    todoText.innerHTML += " " + date.getFullYear() + "-" + (((date.getMonth()+1) < 10)?"0":"") + (date.getMonth()+1) + "-" + 
+    todoCreatedAt.setAttribute("class", "todoCreatedAt");
+    todoCreatedAt.innerHTML = date.getFullYear() + "-" + (((date.getMonth()+1) < 10)?"0":"") + (date.getMonth()+1) + "-" + 
     ((date.getDate() < 10)?"0":"") + date.getDate() + " " + ((date.getHours() < 10)?"0":"") + date.getHours() + ":" + 
     ((date.getMinutes() < 10)?"0":"") + date.getMinutes() + ":" + ((date.getSeconds() < 10)?"0":"") + date.getSeconds(); 
     
+    container.appendChild(todoPriority);
     container.appendChild(todoText);
     container.appendChild(todoCreatedAt);
     li.appendChild(container);
