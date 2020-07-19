69bdb0f2d78decffc2d4bd8eae3b533fa52574e8
shenhars <shenhars03@gmail.com>
2020-07-19 15:09:18 +0300

    add date and time to list item

--- src/main.js
+++ src/main.js
@@ -12,7 +12,15 @@ addButton.addEventListener("click", function() {
     todoText.setAttribute("class", "todoText");
     todoText.innerHTML = textInput.value;
 
+    let todoCreatedAt = document.createElement("span");
+    let date = new Date();
+    todoText.setAttribute("class", "todoCreatedAt");
+    todoText.innerHTML += " " + date.getFullYear() + "-" + (((date.getMonth()+1) < 10)?"0":"") + (date.getMonth()+1) + "-" + 
+    ((date.getDate() < 10)?"0":"") + date.getDate() + " " + ((date.getHours() < 10)?"0":"") + date.getHours() + ":" + 
+    ((date.getMinutes() < 10)?"0":"") + date.getMinutes() + ":" + ((date.getSeconds() < 10)?"0":"") + date.getSeconds(); 
+    
     container.appendChild(todoText);
+    container.appendChild(todoCreatedAt);
     li.appendChild(container);
     toDoList.appendChild(li);
     
